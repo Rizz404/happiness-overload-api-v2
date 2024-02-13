b401bb7d307e1d155286515db45e6bf17eb7bd5a
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2024-02-13 18:08:15 +0700

    ready to merge main

--- src/__tests__/tagRoutes.test.ts
+++ src/__tests__/tagRoutes.test.ts
@@ -2,9 +2,9 @@ import request from "supertest";
 import app from "..";
 
 describe("Tags Routes", () => {
-  describe("GET /tags/", () => {
+  describe("GET /tags?category=all", () => {
     it("Should return arrray of tags", async () => {
-      const res = await request(app).get("/tags");
+      const res = await request(app).get("/tags?category=all");
       const { statusCode, body } = res;
 
       expect(statusCode).toEqual(200);
