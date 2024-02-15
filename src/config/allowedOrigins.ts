ae15fecf81b228bc726d3af25b42783dd6481a77
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2024-02-15 16:10:02 +0700

    add new allowed origin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -5,6 +5,7 @@ const allowedOrigins: string[] = [
   "http://localhost:8000",
   "http://localhost:8080",
   "http://127.0.0.1:8000",
+  "https://social-media-three-livid.vercel.app",
 ];
 
 export default allowedOrigins;
